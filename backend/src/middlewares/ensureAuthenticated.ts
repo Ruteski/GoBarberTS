b19e4943b2ccccfd5f33fe00447fece8394d7e69
Lincoln Ruteski <lruteski@gmail.com>
2021-01-20 15:45:44 -0300

    rotas autenticadas

---
+++ backend/src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+import authConfig from '../config/auth';
+
+interface ITokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error('JWT token is missing');
+  }
+
+  const [, token] = authHeader.split(' ');
+  const { secret } = authConfig.jwt;
+
+  try {
+    const decoded = verify(token, secret);
+
+    const { sub } = decoded as ITokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
+    return next();
+  } catch (err) {
+    throw new Error('Invalid JWT token');
+  }
+}
