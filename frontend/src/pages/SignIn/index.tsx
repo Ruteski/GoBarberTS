770fafed055f21fcefe55a6d87449741382a5e00
Lincoln Ruteski <lruteski@gmail.com>
2021-01-26 13:07:32 -0300

    pagina de login

---
+++ frontend/src/pages/SignIn/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+
+import LogoImg from '../../assets/logo.svg';
+
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={LogoImg} alt="GoBarber" />
+
+      <form>
+        <h1>Faça seu logon</h1>
+
+        <input type="text" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+
+        <button type="submit">Entrar</button>
+
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+
+      <a href="criar">
+        <FiLogIn />
+        Criar conta
+      </a>
+    </Content>
+
+    <Background />
+  </Container>
+);
+
+export default SignIn;
