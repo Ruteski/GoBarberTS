598a093da6db3422f829bcb1db0cf8346377179f
Lincoln Ruteski <lruteski@gmail.com>
2021-01-27 15:51:28 -0300

    mantendo usuário no storage

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useCallback } from 'react';
+/* eslint-disable @typescript-eslint/ban-types */
+import React, { createContext, useCallback, useState } from 'react';
 import api from '../services/api';
 
 interface ISignInCredentials {
@@ -7,24 +8,51 @@ interface ISignInCredentials {
 }
 
 interface IAuthContext {
-  name: string;
   signIn(credentials: ISignInCredentials): Promise<void>;
+  user: object;
 }
+
+interface IAuthState {
+  token: string;
+  user: object;
+}
+
+// interface IResponseSignIn {
+//   token: string;
+//   user: object;
+// }
 
 export const AuthContext = createContext<IAuthContext>({} as IAuthContext);
 
 export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<IAuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as IAuthState;
+  });
+
   const signIn = useCallback(async ({ email, password }) => {
+    // const response = await api.post<IResponseSignIn>('sessions', {
     const response = await api.post('sessions', {
       email,
       password,
     });
 
-    console.log(response.data);
+    const { token, user } = response.data;
+
+    localStorage.setItem('@GoBarber:token', token);
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    setData({ token, user });
   }, []);
 
   return (
-    <AuthContext.Provider value={{ name: 'Lincoln', signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
