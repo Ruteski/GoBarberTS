adcfa08c01ef78265cde11449bea84ad08b86917
Lincoln Ruteski <lruteski@gmail.com>
2021-01-28 12:49:23 -0300

    rotas autenticadas

---
+++ frontend/src/routes/Route.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react/jsx-indent */
+import React from 'react';
+import {
+  Route as ReactDOMRoute,
+  RouteProps as ReactDOMRouteProps,
+  Redirect,
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/Auth';
+
+interface IRouteProps extends ReactDOMRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+const Route: React.FC<IRouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={({ location }) => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+            <Redirect
+              to={{
+                pathname: isPrivate ? '/' : '/dashboard',
+                state: { from: location },
+              }}
+            />
+          );
+      }}
+    />
+  );
+};
+
+export default Route;
