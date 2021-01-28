45017f3c78121e123a7302aee5a375e6c37ecf5c
Lincoln Ruteski <lruteski@gmail.com>
2021-01-28 15:29:05 -0300

    configuração statusbar de forma global

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { View, StatusBar, Text } from 'react-native';
 
 const App: React.FC = () => (
-  <View>
-    <Text>Inicio</Text>
-  </View>
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+      <Text>Inicio</Text>
+    </View>
+  </>
 );
 
 export default App;
