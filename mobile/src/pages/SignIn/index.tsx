e5c7d0774bc3777c7e21997c5ecf4116a43912c1
Lincoln Ruteski <lruteski@gmail.com>
2021-01-29 12:26:17 -0300

    criado componentes de input e button

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Image } from 'react-native';
 
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
 import { Container, Title } from './styles';
 
 import LogoImg from '../../assets/logo.png';
@@ -11,6 +14,17 @@ const SignIn: React.FC = () => {
       <Image source={LogoImg} />
 
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('clicou');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
