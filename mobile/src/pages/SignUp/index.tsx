03a4281acdcc2e606c803223fcefefb76a2ab935
Lincoln Ruteski <lruteski@gmail.com>
2021-02-01 13:34:40 -0300

    cadastro na aplicação

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -14,6 +14,7 @@ import * as Yup from 'yup';
 
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/mobile';
+import api from '../../services/api';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import Button from '../../components/Button';
@@ -52,13 +53,16 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post('/users', data);
+      await api.post('/users', data);
 
       Alert.alert(
         'Cadastro realizado com sucesso',
-        'Você já pode fazer seu Logon no GoBarber',
+        'Você já pode fazer seu Login no GoBarber',
       );
+
+      navigation.goBack();
     } catch (err) {
+      console.log(err);
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
 
